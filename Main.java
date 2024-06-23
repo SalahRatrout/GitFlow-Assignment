2ee07046d9a0be380878b1e998b8ad480ffa045b
Abdel Rahman Jebril <abdjibrel@gmail.com>
2024-06-23 06:50:26 +0300

    create release/release merhaba

--- Main.java
+++ Main.java
@@ -2,13 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("The conflects are resolved");
+        System.out.println("merhaba");
    
     }
+
+
     public int  sum(int a, int b) {
         return a + b;
 
-        System.out.println("Hello Jordan");
     }
 }
