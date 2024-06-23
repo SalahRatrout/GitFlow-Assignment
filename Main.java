44cf510682780b4c337720806e2d4e6b1224ead3
SalahRatrout <81027687+SalahRatrout@users.noreply.github.com>
2024-06-23 07:03:47 +0300

    v1.1

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Assalam Alykoum");
         System.out.println("Alykoum Salam");
+        System.out.println("Merhaba");
     }
 }
