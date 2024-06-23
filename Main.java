793e57fb9907a4dd4896ee92ba7064a428a22de8
Abdel Rahman Jebril <abdjibrel@gmail.com>
2024-06-23 07:16:10 +0300

    create feature/feature3 sum3 fun.

--- Main.java
+++ Main.java
@@ -2,10 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
    
     }
-    public int  sum(int a, int b) {
-        return a + b;
+    public int  sum(int a, int b,int c) {
+        return a + b + c;
     }
 }
