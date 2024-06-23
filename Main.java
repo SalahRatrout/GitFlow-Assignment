047531383cc5681541d3f6db055ecd6f8c9b5498
Abdel Rahman Jebril <abdjibrel@gmail.com>
2024-06-23 07:18:37 +0300

    Commit develop.

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Hello World!");
    
     }
-    public int  sum(int a, int b) {
-        return a + b;
+    public int  sum(int a, int b, int f) {
+        return a + b + f;
     }
 }
